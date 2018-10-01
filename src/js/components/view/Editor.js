567f62e52986627c1532626c1c695ba179497fd4
Dan Parks <dan@eastofcleveland.com>
2018-09-30 19:20:20 -0600

    simple layout of visual blocks for the Editor

--- src/js/components/view/Editor.js
+++ src/js/components/view/Editor.js
@@ -3,9 +3,10 @@ import React from "react";
 class Editor extends React.Component {
   render() {
     return (
-      <div>
-        <h1>Album Cover Editor</h1>
-        <p>TODO: the rest of the app!</p>
+      <div class="flex-container">
+        <div className="panel preview">[preview panel]</div>
+        <div className="panel fields">[title/artist/artwork panel]</div>
+        <div className="bar button-bar">[save/cancel button bar]</div>
       </div>
     );
   }
