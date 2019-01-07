9e2f7e237d53f5a9aa7af681bda7fb4cfa97119d
Dan Parks <dan@eastofcleveland.com>
2019-01-07 09:22:04 -0700

    add TODO for positioning title/artist design elements

--- src/js/components/view/PreviewPanel.js
+++ src/js/components/view/PreviewPanel.js
@@ -8,10 +8,21 @@ const mapStateToProps = state => ({
 });
 
 const PreviewPanel = ({ title, artist, artwork }) => {
+  // TODO: make the positioning of title/artist design elements editable (prob. via click/drag)
+  let titleStyle = {
+    position: "absolute",
+    top: 310 + "px",
+    left: 155 + "px"
+  };
+  let artistStyle = {
+    position: "absolute",
+    top: 15 + "px",
+    left: 40 + "px"
+  };
   return (
     <div className="panel preview">
-      <h1 className="title">{title}</h1>
-      <h2 className="artist">{artist}</h2>
+      <h1 className="title" style={titleStyle}>{title}</h1>
+      <h2 className="artist" style={artistStyle}>{artist}</h2>
       <img className="artwork" src={artwork} />
     </div>
   );
